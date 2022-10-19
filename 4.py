fb8022ce47234bc5bd82e8c15b5cf4e3c904f778
vvilis <liza.gurina999@gmail.com>
2022-10-19 19:32:54 +0300

    dfjfh

---
+++ 4.py
@@ -0,0 +1,16 @@
+
+n = int(input())
+arr = list(map(int,input().split()))
+
+for i in range(1,n):
+    for j in range(i, 0, -1):
+        if arr[j] < arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j-1],arr[j]
+            print(*arr)
+
+        else:
+            break
+
+
+
+
